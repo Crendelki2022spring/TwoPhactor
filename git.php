a6e11517c2720ca624c87cc0af2d673285cb0a14
Kiriql <prorok.kirya@mail.ru>
2022-03-19 12:06:23 +0300

    git

---
+++ git.php
@@ -0,0 +1,48 @@
+<?php
+
+$params = array(
+	'client_id'     => 'f09fcae5ac1048d49bd0',
+	'redirect_uri'  => 'http://hellohahat.beget.tech/oauth-githud.php',
+	'scope'         => 'user',
+	'response_type' => 'code',
+	'state'         => ''
+);
+ 
+$url = 'https://github.com/login/oauth/authorize?' . urldecode(http_build_query($params));
+echo '<a href="' . $url . '">Войти через GitHub</a>';
+
+
+if (!empty($_GET['code'])) {
+	$params = array(
+		'client_id'     => 'Client_ID',
+		'client_secret' => 'Client_secrets',
+		'redirect_uri'  => 'https://ваш_домен/oauth-githud.php',
+		'code'          => $_GET['code']
+	);	
+			
+	$ch = curl_init('https://github.com/login/oauth/access_token');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, urldecode(http_build_query($params))); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	$data = curl_exec($ch);
+	curl_close($ch);	
+	parse_str($data, $data);
+ 
+	if (!empty($data['access_token'])) {
+		$ch = curl_init('https://api.github.com/user');
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: token ' . $data['access_token']));
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$info = curl_exec($ch);
+		curl_close($ch);
+		$info = json_decode($info, true);
+ 
+		if (!empty($info['id'])) {
+			print_r($info);
+		}
+	} 
+}
